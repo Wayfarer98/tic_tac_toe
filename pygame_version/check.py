6924060945eb76c1a1c6d10e6b19776b89911ec0
Keinicke Ubuntu Desktop <magnus@skraentegaarden.dk>
2022-08-11 17:03:22 +0200

    Added previous check functions to this project as well

---
+++ pygame_version/check.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def check_filled(arr: np.ndarray) -> bool:
+    return " " not in arr
+
+def check_win(arr: np.ndarray, symb: str) -> bool:
+    for i in range(3):
+        if np.all(arr[:, i] == symb) or np.all(arr[i, :] == symb):
+            return True
+        if np.all(arr.diagonal() == symb) or np.all(np.fliplr(arr).diagonal() == symb):
+            return True
+    return False
+
+def check_occupied(arr: np.ndarray, row: int, col: int) -> bool:
+        return arr[row, col] != " "
